2e8db43afca43790126abc6b02adb61cf07e6d7f
yaser214 <yaser.alsheikh@ocsbstudent.ca>
2021-01-12 10:52:28 -0500

    Update ColorRange.java
    complete

--- src/com/example/ColorRange.java
+++ src/com/example/ColorRange.java
@@ -10,6 +10,44 @@ import java.util.Scanner;
 public class ColorRange {
 
     public static void main(String args[]) {
+        Scanner scan = new Scanner(System. in);
+        System.out.println("Enter a colour code: ");
+        
+        double waveLength = scan.nextDouble();
+        
+        if(waveLength >380&& waveLength <450) {
+            System.out.println("The colour is violet");
+        }
+        else if (waveLength < 380) {
+            System.out.println("The is not a part of the visible spectrum");   
         
         }
+        else if(waveLength >450&& waveLength <495) {
+            System.out.println("The colour is Blue");
+       
+        }
+        else if(waveLength >495&& waveLength <570) {
+            System.out.println("The colour is Green");
+       
+        }
+        else if(waveLength >570&& waveLength <590) {
+            System.out.println("The colour is Yellow");
+            
+        }
+        else if(waveLength >590&& waveLength <620) {
+            System.out.println("The colour is Orange");
+            
+        }
+        else if(waveLength >620&& waveLength <750) {
+            System.out.println("The colour is Red");
+            
+        }
+        else  {
+        System.out.println("The is not a part of the visible spectrum");  
     }
+    
+        }
+   
+    }
+    
+
